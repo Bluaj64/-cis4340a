1c6aedafbfbf1966815de5513e07233da257257d
Alexander Montealegre <123012542+Bluaj64@users.noreply.github.com>
2024-03-03 23:00:10 -0500

    Rule 12. Thread-Safety Miscellaneous (TSM)

---
+++ R12_TSM02_J.java
@@ -0,0 +1,63 @@
+/* 
+//Noncompliant Code 
+
+public final class ConnectionFactory {
+  private static Connection dbConnection;
+  // Other fields ...
+ 
+  static {
+    Thread dbInitializerThread = new Thread(new Runnable() {
+        @Override public void run() {
+          // Initialize the database connection
+          try {
+            dbConnection = DriverManager.getConnection("connection string");
+          } catch (SQLException e) {
+            dbConnection = null;
+          }
+        }
+    });
+ 
+    // Other initialization, for example, start other threads
+ 
+    dbInitializerThread.start();
+    try {
+      dbInitializerThread.join();
+    } catch (InterruptedException ie) {
+      throw new AssertionError(ie);
+    }
+  }
+ 
+  public static Connection getConnection() {
+    if (dbConnection == null) {
+      throw new IllegalStateException("Error initializing connection");
+    }
+    return dbConnection;
+  }
+ 
+  public static void main(String[] args) {
+    // ...
+    Connection connection = getConnection();
+  }
+}
+
+//Compliant Solution 
+
+public final class ConnectionFactory {
+  private static Connection dbConnection;
+  // Other fields ...
+ 
+  static {
+    // Initialize a database connection
+    try {
+      dbConnection = DriverManager.getConnection("connection string");
+    } catch (SQLException e) {
+      dbConnection = null;
+    }
+    // Other initialization (do not start any threads)
+  }
+ 
+  // ...
+}
+
+}
+*/
